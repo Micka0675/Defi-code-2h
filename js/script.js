25d0e3289617cbae9ce3d9497a471acc5a22247b
Mickael R <94828882+Micka0675@users.noreply.github.com>
2023-01-29 21:38:23 +0100

    update

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@ var retour = document.getElementById('retour');
 var avance = document.getElementById('avant');
 var cursor = 0;
 var icons = document.getElementsByClassName('rogue-leader');
+var contain0 = document.getElementById("container0");
+var contain1 = document.getElementById("container1");
+var contain2 = document.getElementById("container2");
 
 checkTemplate();
 
@@ -11,26 +14,45 @@ retour.addEventListener("click", retourner);
 
 function avancer(){
     cursor++;
+    if(cursor > 2)
+    {
+        cursor = 2;
+    }
+    if(cursor)
     checkTemplate();
 };
 
 function retourner(){
     cursor--;
+    if(cursor < 0)
+    {
+        cursor = 0;
+    }
+    
     checkTemplate();
 };
 
 function checkTemplate(){
+    location.href = "#container" + cursor ;
     for(i=0;i<templates.length;i++)
     {
         if(i != cursor)
         {
-            templates[i].style.display = 'none';
-            icons[i].classList.remove('effect');
+            templates[i].style.opacity = '0';
+            templates[i].style.translate = '50%';
+            templates[i].style.translate = '0%';
+            // templates[i].style.display = 'none';
+            // icons[i].classList.remove('effect');
+
         }
         else
         {
-            templates[i].style.display = 'flex';
-            icons[i].classList.add('effect');
+            templates[i].style.opacity = '1';
+            templates[i].style.translate = '-50%';
+            templates[i].style.translate = '0%';
+            // templates[i].style.display = 'flex';
+            // icons[i].classList.add('effect');
+            
         }
     }
 };
